5b6f6e0848a818cb3d5488a2da3651ed71683ce6
Juan Siagian <jsa.siagian@gmail.com>
2025-06-23 07:17:25 +0700

    feat : template code

---
+++ template.cpp
@@ -0,0 +1,13 @@
+// [Problem]
+// [DD/MM/YYYY] - [HH:MM]
+// JuanSign
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nn '\n'
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+}
